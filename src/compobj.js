c6dce6ce6b322cb71e0f5f04f3ed5dde78908db0
tutu godfrey <godfrey_tutu@yahoo.com>
2017-08-01 15:53:29 +0100

    npmmodulestalled

---
+++ src/compobj.js
@@ -0,0 +1,17 @@
+/*
+ this class definition for management of the and their staffing activities
+ */
+
+class company {
+	constructor (name, location, numstaff){
+	let this.name = name;
+	let this.location = location;
+	let this.numstaff = numstaff;
+	}
+
+	numberofstaff(){}
+
+
+}
+
+
