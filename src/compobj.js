4268e855bcf7bcbe6d4b889bde243829a1d5511a
tutu godfrey <godfrey_tutu@yahoo.com>
2017-08-02 15:17:48 +0100

    editclassname

--- src/compobj.js
+++ src/compobj.js
@@ -1,17 +1,42 @@
+"use strict"
 /*
  this class definition for management of the and their staffing activities
  */
 
-class company {
-	constructor (name, location, numstaff){
-	let this.name = name;
-	let this.location = location;
-	let this.numstaff = numstaff;
+class Company {
+	constructor (compname, complocation, numstaff){
+		 this.compname = compname;
+		 this.complocation = complocation;
+		 this.numstaff = numstaff;
 	}
 
-	numberofstaff(){}
+	companyinfo (){
+		return this.compname + " is located at " + this.complocation + " and " 
+		+ this.compname + " is currently being staffed by a total of this.numstaff.";
+	}
 
+	numberofstaff(){
+		return this.numstaff;
+	}
 
+	complocation (){
+		return this.complocation;
+	}
+
+}
+
+class Staff extends Company {
+	constructor (staffname, staffage, staffpay, staffposition){
+		this.staffname = staffname;
+		this.staffage = staffage;
+		this.staffpay = staffpay;
+		this.staffposition = position;
+	}
+
+	staffinfo(){
+		return this.staffname + " a staff of " + this.compname  + " and " + this.staffname + " is earning " + this.staffpay + " monthly" ;
+
+	}
 }
 
 
